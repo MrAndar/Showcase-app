14e25f0f6556599786a66d546cfca452637e1458
AAndar <r34p3r09@gmail.com>
2024-02-13 18:00:02 +0000

    Implement portrait and description

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/portrait.jpg", width=300)
+
+with col2:
+    st.title("Joe Doe")
+    content = """
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+    when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+    """
+    st.write(content)
+

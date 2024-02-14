1df4f49d19f2c98b52935fcc46d1d7c677bf4a5d
AAndar <r34p3r09@gmail.com>
2024-02-14 18:34:49 +0000

    Implement email form user interface

--- pages/Contact_us.py
+++ pages/Contact_us.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.title("Contact us")
+
+with st.form(key="contact_form"):
+    user_email = st.text_input("Enter Email:")
+    message = st.text_area("Enter message:")
+    submit_btn = st.form_submit_button("Submit")
+    if submit_btn:
+        st.write("Hello")
